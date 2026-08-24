b60fe60d728bbbdc620558add8d3e17d0ce6c51d
yousrik20 <63603356+yousrik20@users.noreply.github.com>
2026-08-24 16:56:39 +0200

    Fix formatting and ensure newline at end of file

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,16 +1,18 @@
 import Header from "components/header/header";
 import React from "react";
 import SigninForm from "./signinForm";
+
 export const metadata = {
   title: "Signin page",
   description: "description for Signin page",
 };
+
 const Page = () => {
   return (
     <>
       <Header isSignPage={true} />
       <main className="px-3">
-      <SigninForm />
+        <SigninForm />
       </main>
     </>
   );
