cf71ce5a6ff31d1d35576383049deff14f885c4e
yousrik20 <63603356+yousrik20@users.noreply.github.com>
2026-08-24 17:08:33 +0200

    Add error handling to user existence check
    Refactor user existence check to include error handling.

--- src/app/api/userExist/route.js
+++ src/app/api/userExist/route.js
@@ -3,20 +3,18 @@ import { connectMongoDB } from "app/DBconfig/mongoDB";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  // 1- Receive data from Front-end
-  const objFromFrontEnd = await request.json();
-  console.log("******************  IS USER EXIST  *************************");
-  console.log(objFromFrontEnd);
+  try {
+    const objFromFrontEnd = await request.json();
 
-  // 2- connect to DB
-  await connectMongoDB();
+    await connectMongoDB();
 
-  // 3- Try to Store obj to DB
-  // @ts-ignore
-  const user = await UserModal.findOne({
-    email: objFromFrontEnd.email,
-  });
+    // @ts-ignore
+    const user = await UserModal.findOne({
+      email: objFromFrontEnd.email,
+    });
 
-  // 4- Go back to frontend
-  return NextResponse.json({ user });
+    return NextResponse.json({ user });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
