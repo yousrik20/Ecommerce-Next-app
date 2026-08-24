90d1fcb0cc23bcc9236b71daac440e1f9caaec29
yousrik20 <63603356+yousrik20@users.noreply.github.com>
2026-08-24 16:57:30 +0200

    Make Page component async and resolve params

--- src/app/(auth)/update-product/[id]/page.jsx
+++ src/app/(auth)/update-product/[id]/page.jsx
@@ -2,12 +2,14 @@ import Header from "components/header/header";
 import React from "react";
 import UpdateForm from "./updateForm";
 
-const Page = ({ params }) => {
+const Page = async ({ params }) => {
+  const resolvedParams = await params;
+
   return (
     <>
       <Header isAdminPage={false} />
       <main className="px-3">
-        <UpdateForm productId={params.id} />
+        <UpdateForm productId={resolvedParams.id} />
       </main>
     </>
   );
